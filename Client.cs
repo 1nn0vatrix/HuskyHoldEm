8626a595d0d2c0222dade04f663c9b608de66fc0
Jenna Martin <jenna90@uw.edu>
2020-02-05 00:30:44 -0800

    Very Basic TCP client/server in C Sharp

---
+++ Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+public class client
+{
+
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient tcpclient = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclient.Connect("127.0.0.1", 8070);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+            Console.Write("Enter the string to be transmitted : ");
+
+            String str = Console.ReadLine();
+            Stream stm = tcpclient.GetStream();
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] sendStream = asen.GetBytes(str);
+            Console.WriteLine("Transmitting.....");
+
+            stm.Write(sendStream, 0, sendStream.Length);
+
+            byte[] byteBuffer = new byte[100];
+            int k = stm.Read(byteBuffer, 0, 100);
+
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(byteBuffer[i]));
+
+            tcpclient.Close();
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+    }
+
+}
---
+++ Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+    public struct threadData
+    {
+    Socket clientID;
+    Thread threadNo;
+    String username;
+        public threadData(Socket sd, Thread td, String un)
+    {
+        this.clientID = sd;
+        this.threadNo = td;
+        this.username = un;
+    }
+    }  
+
+    public class server
+    {
+        static void Main(string[] args)
+        {
+        try
+        {
+            IPAddress addInfo = IPAddress.Parse("127.0.0.1");
+
+            // initialize the listener
+            TcpListener myList = new TcpListener(addInfo, 8070);
+
+            // start listening at the specified port
+            myList.Start();
+
+            Console.WriteLine("The server is running at port 8070");
+            Console.WriteLine("Local Endpoint is: " + myList.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection....");
+
+            // accepting
+            Socket clientSocket = myList.AcceptSocket();
+            Console.WriteLine("Connection accepted from: " + clientSocket.RemoteEndPoint);
+            byte[] buffer = new byte[100];
+            int k = clientSocket.Receive(buffer);
+            Console.WriteLine("Received...");
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(Convert.ToChar(buffer[i]));
+            }
+            ASCIIEncoding asen = new ASCIIEncoding();
+            clientSocket.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
+            /* clean up */
+            clientSocket.Close();
+            myList.Stop();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+    }
+}
