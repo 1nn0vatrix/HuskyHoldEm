3519a31810fb530511b55667ff882e1e9857e564
frankenweenie <anyamixon@gmail.com>
2020-03-12 20:52:06 -0700

    Adds a Bet function to Player.

--- HuskyHoldEm/Player.cs
+++ HuskyHoldEm/Player.cs
@@ -20,5 +20,26 @@ namespace HuskyHoldEm
 			Name = name;
 			Chips = 50;
 		}
+
+		/// <summary>
+		/// Used when a player STAYS (and needs to add to their bet) or RAISES (increases the bet)
+		/// </summary>
+		/// <param name="chipsBet">Amount the player wants to add to the pot.</param>
+		/// <returns>True if they have enough chips to bet, false if they do not or if their bet was zero.</returns>
+		public bool Bet(int chipsBet)
+		{
+			if (chipsBet <= 0 || chipsBet > Chips)
+			{
+				return false;
+			}
+
+			if (Chips - chipsBet >= 0)
+			{
+				Chips -= chipsBet;
+				return true;
+			}
+
+			return false;
+		}
 	}
 }
